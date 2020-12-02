97692491a95872d9fe9ee53224eb4aac80c06fb1
xinmeineo <74601915+xinmeineo@users.noreply.github.com>
2020-12-02 19:53:24 +0800

    Updated theme

---
+++ wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v2.9.0',
+    'version' => '2.9.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7f79bd9b09d2e9fe5e745752fce7db4b42046144',
+    'name' => 'codeinwp/neve',
+  ),
+  'versions' => 
+  array (
+    'codeinwp/neve' => 
+    array (
+      'pretty_version' => 'v2.9.0',
+      'version' => '2.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7f79bd9b09d2e9fe5e745752fce7db4b42046144',
+    ),
+    'codeinwp/themeisle-sdk' => 
+    array (
+      'pretty_version' => '3.2.15',
+      'version' => '3.2.15.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '95b7447a5f4faba410c281c4bf278fbd740fed71',
+    ),
+  ),
+);
