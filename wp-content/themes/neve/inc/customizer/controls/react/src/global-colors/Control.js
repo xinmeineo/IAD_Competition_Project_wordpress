53e5a32cfaefe99c06b5ecb4adceb28e5dd542bc
xinmeineo <74601915+xinmeineo@users.noreply.github.com>
2020-12-02 21:42:18 +0800

    Updated with deleted themes

--- wp-content/themes/neve/inc/customizer/controls/react/src/global-colors/Control.js
+++
@@ -1,8 +0,0 @@
-import GlobalColors from './GlobalColors';
-import { render } from '@wordpress/element';
-
-export const GlobalColorsControl = wp.customize.Control.extend( {
-	renderContent: function renderContent() {
-		render( <GlobalColors control={ this } />, this.container[ 0 ] );
-	},
-} );
